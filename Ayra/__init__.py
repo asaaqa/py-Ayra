aa4e05486ad63218dccb434135e7973bf6530c68
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-21 02:43:00 +0700

    update __init__.py

--- Ayra/__init__.py
+++ Ayra/__init__.py
@@ -93,6 +93,10 @@ if sys.argv[0] == "-m":
     VC_SUDOS = udB.get_key("VC_SUDOS") or "1054295664"
     DUAL_HNDLR = udB.get_key("DUAL_HNDLR") or "/"
     SUDO_HNDLR = udB.get_key("SUDO_HNDLR") or "$"
+    INLINE_PM = udB.set_key("INLINE_PM", "True")
+    PMLOG = udB.set_key("PMLOG", "True")
+    PMSETTING = udB.set_key("PMSETTING", "True")
+    PMWARNS = udB.set_key("PMWARNS", "3")
 else:
     print("Ayra 2022 © senpai80")
 
