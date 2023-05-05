d29d4d37adb56f714cfc3a0e34cfecd74ed8fcf1
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-06 01:38:10 +0700

    update configs.py

--- Ayra/configs.py
+++ Ayra/configs.py
@@ -21,12 +21,12 @@ except ImportError:
 class Var:
     # mandatory
     API_ID = (
-        int(sys.argv[1]) if len(sys.argv) > 1 else config("API_ID", default=6, cast=int)
+        int(sys.argv[1]) if len(sys.argv) > 1 else config("API_ID", default=1634450, cast=int)
     )
     API_HASH = (
         sys.argv[2]
         if len(sys.argv) > 2
-        else config("API_HASH", default=None)
+        else config("API_HASH", default="1a42e816cae8d86e71a4c466bba19b8c")
     )
     SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=None)
     REDIS_URI = (
