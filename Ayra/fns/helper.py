50d12b289ea1a86dd6a0e7bbd49258b691056efb
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-14 00:11:14 +0700

    update helper.py

--- Ayra/fns/helper.py
+++ Ayra/fns/helper.py
@@ -554,6 +554,11 @@ async def restart(ay=None):
                 sys.argv[4],
                 sys.argv[5],
                 sys.argv[6],
+                sys.argv[7],
+                sys.argv[8],
+                sys.argv[9],
+                sys.argv[10],
+                sys.argv[11],
             )
 
 
