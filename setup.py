9478ed8b8a53f74ef3e452caa54b32d99d7e58df
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-06-02 00:21:18 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,8 @@ classifiers = [
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     "Operating System :: OS Independent",
 ]
@@ -44,5 +46,5 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=requirements,
     classifiers=classifiers,
-    python_requires=">3.7, <3.11",
+    python_requires=">3.7, <3.12",
 )
