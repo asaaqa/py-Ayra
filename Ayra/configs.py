2bef6a7ace26bc3dbb66f0295d19329c5406c373
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-08-26 00:28:02 +0700

    asdasd

--- Ayra/configs.py
+++ Ayra/configs.py
@@ -20,28 +20,14 @@ except ImportError:
 
 class Var:
     # mandatory
-    API_ID = (
-        int(sys.argv[1]) if len(sys.argv) > 1 else config("API_ID", default=6, cast=int)
-    )
-    API_HASH = (
-        sys.argv[2]
-        if len(sys.argv) > 2
-        else config("API_HASH", default=None)
-    )
-    SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=None)
-    REDIS_URI = (
-        sys.argv[4]
-        if len(sys.argv) > 4
-        else (config("REDIS_URI", default=None) or config("REDIS_URL", default=None))
-    )
-    REDIS_PASSWORD = (
-        sys.argv[5] if len(sys.argv) > 5 else config("REDIS_PASSWORD", default=None)
-    )
-    MONGO_URI = (
-        sys.argv[6] if len(sys.argv) > 6 else config("MONGO_URI", default=None)
-    )
+    API_ID = config("API_ID", default=6, cast=int)
+    API_HASH = config("API_HASH", default=None)
+    SESSION = config("SESSION", default=None)
+    REDIS_URI = config("REDIS_URL", default=None)
+    REDIS_PASSWORD = config("REDIS_PASSWORD", default=None)
+    MONGO_URI = config("MONGO_URI", default=None)
     BOT_TOKEN = config("BOT_TOKEN", default=None)
-    LOG_CHANNEL = config("LOG_CHANNEL", default=0, cast=int)
+    LOG_CHANNEL = config("LOG_CHANNEL", default=123, cast=int)
     HEROKU_APP_NAME = config("HEROKU_APP_NAME", default=None)
     HEROKU_API = config("HEROKU_API", default=None)
     SUDO = config("SUDO", default=True, cast=bool)
@@ -50,10 +36,3 @@ class Var:
     INLINE_PIC = config("INLINE_PIC", default=False, cast=bool)
     VCBOT = config("VCBOT", default=True, cast=bool)
     DISABLE_PMDEL = config("DISABLE_PMDEL", default=True, cast=bool)
-    # for railway
-    REDISPASSWORD = config("REDISPASSWORD", default=None)
-    REDISHOST = config("REDISHOST", default=None)
-    REDISPORT = config("REDISPORT", default=None)
-    REDISUSER = config("REDISUSER", default=None)
-    # for sql
-    DATABASE_URL = config("DATABASE_URL", default=None)
--- Ayra/version.py
+++ Ayra/version.py
@@ -1,2 +1,2 @@
-__version__ = "1.9.3"
+__version__ = "1.9.4"
 ayra_version = "2.0.4"
